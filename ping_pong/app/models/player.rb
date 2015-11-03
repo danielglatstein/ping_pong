04d349a65e318b267df0852b4350f82de0d3adc2
danielglatstein <danielglatstein@gmail.com>
2015-11-03 16:46:22 -0500

    merge player_show

--- ping_pong/app/models/player.rb
+++ ping_pong/app/models/player.rb
@@ -17,28 +17,5 @@ class Player < ActiveRecord::Base
   validates_uniqueness_of :email
   # has_secure_password
 
-  def teams
-    select from teams.* all games where player_1_id == self.id
-    # all games for self = player1_games + player2_games 
-    # [return should be a collection of instances where self is player1 and player2]
-  end
 
-  def games
-    #[return collection of all games self's teams were involved in]
-  end
-
-  def wins_single
-    single_games.select do |game|
-       game == win
-    end
-  end
-
-  def losses_single
-  end
-
-  def single_games
-  end
-
-  def doubles_games
-  end
 end
